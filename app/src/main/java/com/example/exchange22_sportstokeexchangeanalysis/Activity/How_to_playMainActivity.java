f94870c9d343a852adeec19d6791b9610a00f60f
omshivay <sandipmori383@gmail.com>
2021-07-20 19:02:26 +0530

    first new new new

---
+++ app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Activity/How_to_playMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.exchange22_sportstokeexchangeanalysis.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.exchange22_sportstokeexchangeanalysis.R;
+
+public class How_to_playMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_main);
+    }
+}
--- app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Fragment/Guide_Fragment.java
+++ app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Fragment/Guide_Fragment.java
@@ -2,6 +2,7 @@ package com.example.exchange22_sportstokeexchangeanalysis.Fragment;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
