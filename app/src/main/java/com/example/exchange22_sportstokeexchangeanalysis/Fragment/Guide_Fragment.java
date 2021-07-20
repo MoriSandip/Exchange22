415e2485a082612694530755edc08bbf8cc8f370
ravibhadani <ravibhadanidevkrushna@gmail.com>
2021-07-20 19:02:53 +0530

    second

--- app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Fragment/Guide_Fragment.java
+++ app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Fragment/Guide_Fragment.java
@@ -1,7 +1,9 @@
 package com.example.exchange22_sportstokeexchangeanalysis.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,13 +11,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.exchange22_sportstokeexchangeanalysis.R;
+import com.example.exchange22_sportstokeexchangeanalysis.databinding.FragmentGuideBinding;
 
 
 public class Guide_Fragment extends Fragment {
-
+    FragmentGuideBinding binding;
+ CardView cardview;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_guide_, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentGuideBinding.inflate(inflater, container, false);
+
+        binding.howToPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplication(), MainActivityList.class);
+                startActivity(intent);
+            }
+        });
+        return binding.getRoot();
     }
 }
