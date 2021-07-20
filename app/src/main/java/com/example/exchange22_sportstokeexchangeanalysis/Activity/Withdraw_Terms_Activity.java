c4ee7688b521fee9c9ea09d4b7029f6a2b07c2a4
omshivay <sandipmori383@gmail.com>
2021-07-20 14:58:21 +0530

    first

---
+++ app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Activity/Withdraw_Terms_Activity.java
@@ -0,0 +1,16 @@
+package com.example.exchange22_sportstokeexchangeanalysis.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.exchange22_sportstokeexchangeanalysis.R;
+
+public class Withdraw_Terms_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_withdraw_terms);
+    }
+}
