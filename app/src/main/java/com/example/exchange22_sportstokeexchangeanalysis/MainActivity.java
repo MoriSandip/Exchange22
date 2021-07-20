7cfbff3139bcede1b239d1837367557583b0c7b9
omshivay <sandipmori383@gmail.com>
2021-07-20 10:13:08 +0530

    first

---
+++ app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.exchange22_sportstokeexchangeanalysis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
