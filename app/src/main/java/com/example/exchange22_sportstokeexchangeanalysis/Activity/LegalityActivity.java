25a99a09bd698ff1946d26ca983ac55ce8be0e8e
omshivay <sandipmori383@gmail.com>
2021-07-21 10:24:44 +0530

    first new new new new new

--- app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Activity/LegalityActivity.java
+++ app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Activity/LegalityActivity.java
@@ -14,14 +14,23 @@ import com.example.exchange22_sportstokeexchangeanalysis.R;
 
 public class LegalityActivity extends AppCompatActivity {
 
-    ImageView imageView14;
-    CardView cardView;
+    ImageView imageView14,imageView15,imageView16,imageView17,imageView18;
+    CardView cardView,cardView1,cardView2,cardView3,cardView4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_legality);
         imageView14 = findViewById(R.id.imageView14);
+        imageView15 = findViewById(R.id.imageView15);
+        imageView16 = findViewById(R.id.imageView16);
+        imageView17 = findViewById(R.id.imageView17);
+        imageView18 = findViewById(R.id.imageView18);
         cardView = findViewById(R.id.cardView21);
+        cardView1 = findViewById(R.id.cardView22);
+        cardView2 = findViewById(R.id.cardView23);
+        cardView3 = findViewById(R.id.cardView24);
+        cardView4 = findViewById(R.id.cardView25);
+
         imageView14.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +49,98 @@ public class LegalityActivity extends AppCompatActivity {
                     cardView.setVisibility(View.VISIBLE);
 
                     imageView14.setImageResource(R.drawable.ic_bottom_foreground);
+
+
+                }
+            }
+        });
+        imageView15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( cardView1.getVisibility() == View.VISIBLE) {
+
+
+                    TransitionManager.beginDelayedTransition(cardView1, new AutoTransition());
+
+                    cardView1.setVisibility(View.GONE);
+                    imageView15.setImageResource(R.drawable.ic_bottom_top_foreground);
+
+                } else {
+
+                    TransitionManager.beginDelayedTransition(cardView1, new AutoTransition());
+
+                    cardView1.setVisibility(View.VISIBLE);
+
+                    imageView15.setImageResource(R.drawable.ic_bottom_foreground);
+
+
+                }
+            }
+        });
+        imageView16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( cardView2.getVisibility() == View.VISIBLE) {
+
+
+                    TransitionManager.beginDelayedTransition(cardView2, new AutoTransition());
+
+                    cardView2.setVisibility(View.GONE);
+                    imageView16.setImageResource(R.drawable.ic_bottom_top_foreground);
+
+                } else {
+
+                    TransitionManager.beginDelayedTransition(cardView2, new AutoTransition());
+
+                    cardView2.setVisibility(View.VISIBLE);
+
+                    imageView16.setImageResource(R.drawable.ic_bottom_foreground);
+
+
+                }
+            }
+        });
+        imageView17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( cardView3.getVisibility() == View.VISIBLE) {
+
+
+                    TransitionManager.beginDelayedTransition(cardView3, new AutoTransition());
+
+                    cardView3.setVisibility(View.GONE);
+                    imageView17.setImageResource(R.drawable.ic_bottom_top_foreground);
+
+                } else {
+
+                    TransitionManager.beginDelayedTransition(cardView3, new AutoTransition());
+
+                    cardView3.setVisibility(View.VISIBLE);
+
+                    imageView17.setImageResource(R.drawable.ic_bottom_foreground);
+
+
+                }
+            }
+        });
+        imageView18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( cardView4.getVisibility() == View.VISIBLE) {
+
+
+                    TransitionManager.beginDelayedTransition(cardView4, new AutoTransition());
+
+                    cardView4.setVisibility(View.GONE);
+                    imageView18.setImageResource(R.drawable.ic_bottom_top_foreground);
+
+                } else {
+
+                    TransitionManager.beginDelayedTransition(cardView4, new AutoTransition());
+
+                    cardView4.setVisibility(View.VISIBLE);
+
+                    imageView18.setImageResource(R.drawable.ic_bottom_foreground);
 
 
                 }
--- app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Fragment/Guide_Fragment.java
+++ app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Fragment/Guide_Fragment.java
@@ -11,15 +11,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.exchange22_sportstokeexchangeanalysis.Activity.About_Activity;
 import com.example.exchange22_sportstokeexchangeanalysis.Activity.FeaturesActivity;
 import com.example.exchange22_sportstokeexchangeanalysis.Activity.How_to_playMainActivity;
 import com.example.exchange22_sportstokeexchangeanalysis.Activity.LegalityActivity;
+import com.example.exchange22_sportstokeexchangeanalysis.Activity.Withdraw_Terms_Activity;
 import com.example.exchange22_sportstokeexchangeanalysis.R;
 
 
 public class Guide_Fragment extends Fragment {
 
-    CardView how_to_play,feature,cardView5;
+    CardView how_to_play,feature,cardView5,cardView10,cardView6;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -29,11 +31,29 @@ public class Guide_Fragment extends Fragment {
         how_to_play = view.findViewById(R.id.how_to_play);
         feature = view.findViewById(R.id.feature);
         cardView5 = view.findViewById(R.id.cardView5);
+        cardView10 = view.findViewById(R.id.cardView10);
+        cardView6 = view.findViewById(R.id.cardView6);
 
         how_to_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                startActivity(new Intent(getContext(), How_to_playMainActivity.class));
+                Toast.makeText(getContext(), "adgdfg", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        cardView10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), About_Activity.class));
+                Toast.makeText(getContext(), "adgdfg", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        cardView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), Withdraw_Terms_Activity.class));
                 Toast.makeText(getContext(), "adgdfg", Toast.LENGTH_SHORT).show();
 
             }
