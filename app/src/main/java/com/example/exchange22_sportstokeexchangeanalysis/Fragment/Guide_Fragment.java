4168832bc0c7e24bfe2bc8b12c41e4ae343c63d6
ravibhadani <ravibhadanidevkrushna@gmail.com>
2021-07-21 10:06:30 +0530

    second

--- app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Fragment/Guide_Fragment.java
+++ app/src/main/java/com/example/exchange22_sportstokeexchangeanalysis/Fragment/Guide_Fragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.exchange22_sportstokeexchangeanalysis.Activity.How_to_playMainActivity;
 import com.example.exchange22_sportstokeexchangeanalysis.R;
 import com.example.exchange22_sportstokeexchangeanalysis.databinding.FragmentGuideBinding;
 
@@ -21,13 +22,13 @@ public class Guide_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentGuideBinding.inflate(inflater, container, false);
 
-        binding.howToPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity().getApplication(), MainActivityList.class);
-                startActivity(intent);
-            }
+        binding.howToPlay.setOnClickListener(v -> {
+           Howtoplay();
         });
         return binding.getRoot();
+    }
+    public void Howtoplay() {
+        Intent intent = new Intent(getActivity(), How_to_playMainActivity.class);
+        startActivity(intent);
     }
 }
